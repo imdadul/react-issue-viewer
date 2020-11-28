29891efd33fb6e55d03932c89a5eb970d063b8f7
imdadul <imdadul.huq3@gmail.com>
2020-11-28 17:08:14 +0100

    feat: project created with webpack conf

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App = () => {
+    const asd: number = 4;
+    return <>My React Imdad {asd}</>
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const rootDir = path.resolve(__dirname, '..');
+module.exports = {
+    entry: {
+        main: ['./src/index.tsx'],
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+            '.json',
+            '.ts',
+            '.tsx',
+            '.spec.ts',
+            '.spec.tsx',
+            '.d.ts',
+            '.graphql',
+            '.gql',
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { 
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: "[name]__[local]___[hash:base64:5]",
+                            },														
+                            sourceMap: true
+                        }
+                     }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=10000&name=img/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/public/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
