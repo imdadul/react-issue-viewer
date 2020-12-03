3c68a56b18a6b3b439b3b063ec4d671d11a859c6
imdadul <imdadul.huq3@gmail.com>
2020-12-03 21:20:03 +0100

    feat: webpack prod & port updated

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+app.set('port', process.env.PORT || 9000);
 
 var server = app.listen(app.get('port'), function() {
     console.log('listening on port ', server.address().port);
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,12 +4,6 @@ const common = require('./webpack.common.js');
 const path = require('path');
 module.exports = merge(common, {
     mode: 'production',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        historyApiFallback: true,
-        compress: true,
-        port: 9000
-    },
     plugins: [
         new EnvironmentPlugin(['REACT_APP_API_URL', 'REACT_APP_TOKEN'])
     ],
